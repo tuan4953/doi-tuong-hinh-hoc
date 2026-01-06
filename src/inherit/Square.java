9b7e800c1ef309760d410770c1d71c19fdfe12b2
tuan4953 <hangle19902021@gmail.com>
2026-01-06 11:27:04 +0700

    first commit

---
+++ src/inherit/Square.java
@@ -0,0 +1,45 @@
+package inherit;
+
+public class Square extends Rectangle {
+
+    public Square() {
+        super(1.0, 1.0);
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
+
+    public void setSide(double side) {
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+        super.setLength(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setWidth(length);
+        super.setLength(length);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
